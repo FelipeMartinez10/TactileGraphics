49e3f6c8e1a14834fb7e707d4c4221ee0245951f
Felipe Martinez Piazuelo <f.martinez11@uniandes.edu.co>
2018-04-13 14:18:03 -0500

    fix error with process.env.

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const AutoMLURL = "https://alpha-vision.googleapis.com/v1/images:annotate"
 const BucketURL = "https://www.googleapis.com/upload/storage/v1/b/custom-search-images/o?uploadType=media&name="
 const http = require('http');
 const https = require('https');
-//const key = require('../private/key.json');
-const key = process.env;
+const key = require('../private/key.json');
+//const key = process.env;
 
 
 const Model1 = "projects/ml-for-tactile-graphics/models/Tactile_graphics/versions/Tactile_graphics_201802221336_base";

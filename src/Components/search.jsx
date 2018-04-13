7f889bfe5275298ee980639e9b3d229e0c5cd5c2
Felipe Martinez Piazuelo <f.martinez11@uniandes.edu.co>
2018-04-13 13:37:47 -0500

    change to envs

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import '../App.css';
 import axios from 'axios';
 import Image from './image.jsx';
-import frontVariables from '../privateKeys/frontVariables.js'
+//import frontVariables from '../privateKeys/frontVariables.js'
 import { BarLoader } from 'react-spinners';
 
-const URL = "https://www.googleapis.com/customsearch/v1?key="+frontVariables.key+"&cx=004485904051950933441:5x_3_wemizq&q="
+//const URL = "https://www.googleapis.com/customsearch/v1?key="+frontVariables.key+"&cx=004485904051950933441:5x_3_wemizq&q="
+const URL = "https://www.googleapis.com/customsearch/v1?key="+process.env.CUSTOM_SEARCH+"&cx=004485904051950933441:5x_3_wemizq&q="
 const serverURL = "http://localhost:3333"
 
 class Search extends Component {
